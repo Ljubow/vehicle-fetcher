09478361930f84746e5718f3f79778b43042b558
Ljubow <lubovobrusnik@gmail.com>
2017-09-06 15:43:17 +0300

    feature/vehicle-fetcher

--- js/index.js
+++ js/index.js
@@ -7,20 +7,24 @@ request.onreadystatechange = function() {
         let myArr = JSON.parse(this.responseText);
         let myArrs = myArr.items;
         for (let i = 0; i < myArrs.length; i++) {
-            content += `<table class='table'>`+
+            content +=
+            `<table class='table-sm'>`+
+            `<tr class="table-danger">`+
                        `<tbody>`+
-                       `<td><img src='${myArrs[i].volumeInfo.imageLinks.thumbnail}' alt='Card image cap'></td>`+
-                       `<tr><td>Name</td><td><div>${myArrs[i].volumeInfo.title}</div></td></tr>`+
-                       `<tr><td>Authors</td><td><div>${myArrs[i].volumeInfo.authors}</div></td></tr>`+
-                       `<tr><td>Publisher</td><td><div>${myArrs[i].volumeInfo.publisher}</div></td></tr>`+
-                       `<tr><td>Publication date</td><td><div>${myArrs[i].volumeInfo.publishedDate}</div></td></tr>`+
-                       `<tr><td>Page count</td><td><div>${myArrs[i].volumeInfo.pageCount}</div></td></tr>`+
-                       `<tr><td>Categories</td><td><div>${myArrs[i].volumeInfo.categories}</div></td></tr>`+
-                       `<tr><td>Description</td><td><div>${myArrs[i].volumeInfo.description}</div></td></tr>`+
+
+                       `<tr><td><img src='${myArrs[i].volumeInfo.imageLinks.thumbnail}'  class="rounded float-left" alt='Card image cap'></td>`+
+                       `<td><div class='row'><div class='col-sm-12'>Name:</div><div class='col-sm-12'>${myArrs[i].volumeInfo.title}</div></div></td></tr>`+
+                       `<tr><td>Authors:</td><td><div>${myArrs[i].volumeInfo.authors}</div></td></tr>`+
+                       `<tr><td>Publisher:</td><td><div>${myArrs[i].volumeInfo.publisher}</div></td></tr>`+
+                       `<tr><td>Publication date:</td><td><div>${myArrs[i].volumeInfo.publishedDate}</div></td></tr>`+
+                       `<tr><td>Page count:</td><td><div>${myArrs[i].volumeInfo.pageCount}</div></td></tr>`+
+                       `<tr><td>Categories:</td><td><div>${myArrs[i].volumeInfo.categories}</div></td></tr>`+
+                       `<tr><td>Description:</td><td><div>${myArrs[i].volumeInfo.description}</div></td></tr>`+
+
                        `</tbody>`+
+                       `</tr>`+
                        `</table>`
   }
-console.log(content);
         const elem = document.getElementById("text");
         elem.innerHTML = content;
 }
