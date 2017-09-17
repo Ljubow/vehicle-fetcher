375407c9e071ec0bc95052ac9bc59d6240e7bafa
Ljubow <lubovobrusnik@gmail.com>
2017-09-17 12:35:44 +0300

    feature/vehicle-fetcher

--- js/index.js
+++ js/index.js
@@ -1,62 +1,59 @@
-const ready = () => {
     const request = new XMLHttpRequest;
     const url = 'https://www.googleapis.com/books/v1/volumes?q=test:keyes&key=AIzaSyCQHwYgHthKdQzPFjB7FZ7JCmEE6UxOtM8';
-
     request.onreadystatechange = function() {
-        console.log(this.responseText);
+
         if (this.readyState == 4 && this.status == 200) {
             let content = '';
-            let myArr = JSON.parse(this.responseText);
-            let myArrs = myArr.items;
-            for (let i = 0; i < myArrs.length; i++){
-              let data = {
-                id:myArrs[i].id,
-                img: myArrs[i].volumeInfo.imageLinks.thumbnail || '',
-                title: myArrs[i].volumeInfo.title || 'Nameless',
-                authors: myArrs[i].volumeInfo.authors || 'Unknown',
-                categories: myArrs[i].volumeInfo.categories || 'No categories',
-                pageCount: myArrs[i].volumeInfo.pageCount || 'Unknown',
-                publishedDate: myArrs[i].volumeInfo.publishedDate || 'Unknown',
-                publisher: myArrs[i].volumeInfo.publisher || 'Unknown',
-                description: myArrs[i].volumeInfo.description || 'No description',
+            console.log(JSON.parse(this.responseText))
+            let myArrs = JSON.parse(this.responseText).items;
+     myArrs.map((item) => {
+                let data = {
+                    id: item.id,
+                    img: (!item.volumeInfo.imageLinks || !item.volumeInfo.imageLinks.thumbnail) ?
+                    'img/content.png' : item.volumeInfo.imageLinks.thumbnail,
+                    title: item.volumeInfo.title || 'Nameless',
+                    authors: (item.volumeInfo.authors) ? item.volumeInfo.authors.join(', ') : 'Unknown',
+                    categories: (item.volumeInfo.categories) ? item.volumeInfo.categories.join(', ') : 'No categories',
+                    pageCount: item.volumeInfo.pageCount || 'Unknown',
+                    publishedDate: item.volumeInfo.publishedDate || 'Unknown',
+                    publisher: item.volumeInfo.publisher || 'Unknown',
+                    description: item.volumeInfo.description || 'No description',
 
-              };
-                content += `<div class='card-columns'>` +
-                    `<div class='card' style="width: 300px;">` +
-                    `<img class="rounded mx-auto d-block" src='${data.img}'` +
-                    `width="170" height="230" alt='Card image'>` +
-                    `<div class='card-block text-right'` +
-                    `<div class="list-group">` +
-                    `<a href="#" class="list-group-item">` +
-                    `Name:<div>${data.title}</div></a>` +
-                    `<a href="#" class="list-group-item"><div class='row'><div class='col-sm-6'>Authors:` +
-                    `</div><div class='col-sm-6'>${data.authors}</div></div></a>` +
-                    `<a href="#" class="list-group-item"><div class='row'><div class='col-sm-6'>Categories:` +
-                    `</div><div class='col-sm-6'>${data.categories}</div></div></a>` +
-                    `<div class="card-footer">` +
-                    `<button type='button' data-toggle='collapse'` +
-                    `data-target='#${myArrs[i].id}'>More information</button>` +
-                    `</div>` +
-                    `<div id='${myArrs[i].id}' class='collapse'>` +
-                    `<a href="#" class="list-group-item"><div class='row'><div class='col-sm-6'>Published Date:` +
-                    `</div><div class='col-sm-6'>${data.publishedDate}</div></div></a>` +
-                    `<a href="#" class="list-group-item"><div class='row'><div class='col-sm-6'>Page count:` +
-                    `</div><div class='col-sm-6'>${data.pageCount}</div></div></a>` +
-                    `<a href="#" class="list-group-item"><div class='row'><div class='col-sm-6'>Publisher:` +
-                    `</div><div class='col-sm-6'>${data.publisher}</div></div></a>` +
-                    `<a href="#" class="list-group-item">Description:` +
-                    `<div>${data.description}</div></a>` +
-                    `</div>` +
-                    `</div>` +
-                    `</div>` +
-                    `</div>` +
-                    `</div>`
-            }
-            const elem = document.getElementById("text");
+                };
+                let header = `<div class='book-heading'>
+                                  <h2 class='text-center'>
+                                      ${data.title}
+                                  </h2>
+                                  <img class='rounded center-block' src='${data.img}'>
+                              </div>`;
+                let body = `<div class='book-body'>
+                                <div class='primary-info'>
+                                    <p><b>Authors</b>: ${data.authors}</p>
+                                    <p><b>Categories</b>: ${data.categories}</p>
+                                </div>
+                                <div class='container-fluid'>
+                                    <button class='btn btn-info' data-toggle='collapse'
+                                    data-target='#${data.id}'>More information</button>
+                                </div>
+                            </div>`;
+                let footer = `<div id='${data.id}' class='book-footer collapse'>
+                                  <p><b>Date</b>: ${data.publishedDate}</p>
+                                  <p><b>Publisher</b>: ${data.publisher}</p>
+                                  <p><b>Page count</b>: ${data.pageCount}</p>
+                                  <p><b>Description</b>: ${data.description}</p>
+                              </div>`;
+
+                content +=
+                    `<div class='col-xs-12 col-sd-6 col-md-4 col-lg-3'>
+                        ${header}
+                        ${body}
+                        ${footer}
+                    </div>`
+       })
+
+            const elem = document.querySelector('.row');
             elem.innerHTML = content;
-  }
-}
+        }
+    }
     request.open('GET', url, true);
     request.send();
-}
-document.addEventListener('DOMContentLoaded', ready);
